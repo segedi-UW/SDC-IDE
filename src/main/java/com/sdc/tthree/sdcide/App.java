4146981d4a53af00a75f1fd1ebb24bea04e4ec2d
Anthony Segedi <79811294+segedi-UW@users.noreply.github.com>
2021-10-11 01:59:38 -0500

    Refactor method name for readability

--- src/main/java/com/sdc/tthree/sdcide/App.java
+++ src/main/java/com/sdc/tthree/sdcide/App.java
@@ -13,7 +13,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = fxmlLoader("Controller.fxml");
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        addStylesheet(scene);
+        addDefaultStylesheet(scene);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -23,13 +23,13 @@ public class App extends Application {
         return new FXMLLoader(resourceURL(resource));
     }
 
-    public static void addStylesheet(Scene scene) {
+    public static void addDefaultStylesheet(Scene scene) {
         scene.getStylesheets().add(resourceURL("stylesheet.css").toExternalForm());
     }
 
     public static URL resourceURL(String resource) {
         URL url = App.class.getResource(resource);
-        if (url == null) throw new NullPointerException("Resource \"" + resource + "\" was not found");
+        if (url == null) throw new NullPointerException(String.format("Resource \"%s\" was not found", resource));
         return url;
     }
 
