98dab1824f649998eac5dc89ca5abe902488196b
Anthony Segedi <79811294+segedi-UW@users.noreply.github.com>
2021-10-11 00:18:48 -0500

    Initial Project Setup

--- src/main/java/com/sdc/tthree/sdcide/Controller.java
+++ src/main/java/com/sdc/tthree/sdcide/Controller.java
@@ -3,9 +3,14 @@ package com.sdc.tthree.sdcide;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private void initialize() {
+        // TODO
+    }
 
     @FXML
     protected void onHelloButtonClick() {
